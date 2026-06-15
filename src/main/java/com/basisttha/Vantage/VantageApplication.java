f39879f17a6416ee49d237062a8bb6d8cd1e0cf9
basisthakr <basisthakum@proton.me>
2026-06-15 09:04:51 +0530

    Initial Commit
    Initial Commit

---
+++ src/main/java/com/basisttha/Vantage/VantageApplication.java
@@ -0,0 +1,13 @@
+package com.basisttha.Vantage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VantageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VantageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basisttha/Vantage/VantageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basisttha.Vantage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VantageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
